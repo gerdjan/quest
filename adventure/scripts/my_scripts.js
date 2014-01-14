3823a944ce152ce10d1385ef9e301e405e3e0b11
gerdjan <christiaanwapenaar@hotmail.com>
2014-01-14 15:13:08 +0100

    added the first file

---
+++ adventure/scripts/my_scripts.js
@@ -0,0 +1,11 @@
+/*var naam;
+naam=window.prompt("Hoe heet u, o grote avonturier??? ");
+if (naam=="" || naam=="Vul uw naam in dit veld in")
+{
+    document.write("player");
+}
+else
+{
+  document.write(naam);
+}
+*/
